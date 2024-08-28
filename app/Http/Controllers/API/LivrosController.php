aea6348a1729338999cebcebf8c35143ec3b4761
Samuel Dourado <sd7038@gmail.com>
2024-08-28 18:32:13 -0300

    implementa endpoint para ler,criar, editar e excluir livros

--- app/Http/Controllers/API/LivrosController.php
+++ app/Http/Controllers/API/LivrosController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LivroPostRequest;
+use App\Http\Requests\LivroUpdateRequest;
 use App\Http\Resources\LivroCollection;
+use App\Http\Resources\LivroResource;
 use App\Models\Livro;
 use Illuminate\Http\Request;
 
@@ -11,6 +14,29 @@ class LivrosController extends Controller
 {
     public function index()
     {
-        return new LivroCollection(Livro::all());
+        return new LivroCollection(Livro::paginate(10));
+    }
+
+    public function show(Livro $livro)
+    {
+        return new LivroResource($livro);
+    }
+
+    public function store(LivroPostRequest $request)
+    {
+        $userData = $request->validated();
+        $livro = Livro::create($userData);
+        return new LivroResource($livro);
+    }
+
+    public function update(LivroUpdateRequest $request, Livro $livro)
+    {
+        $livro->update($request->validated());
+    }
+
+    public function destroy(Livro $livro)
+    {
+        $livro->delete();
+        return response()->noContent();
     }
 }
--- app/Models/Livro.php
+++ app/Models/Livro.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Livro extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'codL',
+        'titulo',
+        'editora',
+        'edicao',
+        'anoPublicacao'
+    ];
 }
